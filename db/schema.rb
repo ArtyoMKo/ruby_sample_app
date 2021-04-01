bf3be4abb6ce4db3e0bf00beb7e8cf764576a23c
ArtyoMKo <artyomkosakyan97@gmail.com>
2021-04-01 22:57:28 +0400

    first

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_152545) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
