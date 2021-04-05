256ef92e7a80f67478f942b1923818ec294e4780
ArtyoMKo <artyomkosakyan97@gmail.com>
2021-04-05 14:34:32 +0400

    users updating system

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
+  def setup
+    @user = users(:michael)
+    @outher_user = users(:archer)
+  end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
   end
 end
